ebefe9174c2ce6c8598f5e9218693ae8841e34de
Dr0wsyC0der <dodopizza24@outlook.com>
2026-01-18 21:15:12 +0300

    add application lifecycle and alive services

--- backend/core/lifecycle.py
+++ backend/core/lifecycle.py
@@ -1,2 +1,21 @@
-class AppContext:
-    bus_service = None
+from core.event_bus import EventBus
+from core.alive_services import AppContext
+from modules.buses.service import BusService
+from modules.output.console import ConsoleOutput
+
+async def boot() -> EventBus:
+    bus = EventBus()
+
+    console = ConsoleOutput(show_album=True)
+    bus.subscribe("track_changed", console.on_track)
+    bus.subscribe("volume_changed", console.on_volume)
+    bus.subscribe("big_system_load", console.on_load)
+
+    bus_service = BusService()
+    try:
+        await bus_service.update_cache()
+    except Exception as e:
+        print("Не удалось обновить расписание:", e)
+    AppContext.bus_service = bus_service
+
+    return bus
--- backend/main.py
+++ backend/main.py
@@ -1,28 +1,11 @@
 import asyncio
-from backend.core.event_bus import EventBus
-from backend.modules.music.service import MediaPlayerService
-from backend.modules.volume.service import VolumeService
-from backend.modules.system_monitor.service import AsyncSystemMonitor
-from backend.modules.output.console import ConsoleOutput
-from backend.modules.buses.service import BusService
-from backend.core.lifecycle import AppContext
+from core.lifecycle import boot
+from modules.music.service import MediaPlayerService
+from modules.volume.service import VolumeService
+from modules.system_monitor.service import AsyncSystemMonitor
 
 async def main():
-    bus = EventBus()
-
-    console = ConsoleOutput(show_album=True)
-
-    bus.subscribe("track_changed", console.on_track)
-    bus.subscribe("volume_changed", console.on_volume)
-    bus.subscribe("big_system_load", console.on_load)
-
-
-    bus_service = BusService()
-    try:
-        await bus_service.update_cache()
-    except Exception as e:
-        print("Не удалось обновить расписание:", e)
-    AppContext.bus_service = bus_service
+    bus = await boot()
 
     await asyncio.gather(
         MediaPlayerService(bus).start(),
@@ -30,4 +13,5 @@ async def main():
         AsyncSystemMonitor(bus).start()
     )
 
-asyncio.run(main())
+if __name__ == "__main__":
+    asyncio.run(main())
